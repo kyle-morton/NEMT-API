6ea15562450268b5e5015d95ea5b260a5206110e
Kyle Morton <kylemortondevelopment@gmail.com>
2019-04-29 13:02:10 -0500

    added delete route

--- app/handlers/contact.handler.ts
+++ app/handlers/contact.handler.ts
@@ -6,14 +6,43 @@ const Contact = mongoose.model('Contact', ContactSchema);
 
 export class ContactHandler {
 
+    /**
+     * Create new contact document
+     * @param body 
+     */
     public async create (body: any) {
 
         const newContact = new Contact(body);
-
         await newContact.save();      
-
         return newContact;
 
+    }
+
+    /**
+     * get contact document(s) based on filter
+     * @param filter 
+     */
+    public async get (filter: any) {
+        return await Contact.find(filter)
+    }
+
+    /**
+     * update contact
+     * @param id 
+     * @param body 
+     */
+    public async update (id: string, body: any) {
+        return await Contact.findOneAndUpdate(
+            { _id: id },
+            body,
+            { new: true }
+        );
+    }
+
+    public async delete (id: string) {
+        return await Contact.findOneAndDelete(
+            { _id: id}
+        );
     }
     
 };
--- app/routes/contact.router.ts
+++ app/routes/contact.router.ts
@@ -5,52 +5,51 @@ export class ContactRoutes {
 
     public contactHandler : ContactHandler = new ContactHandler();
 
+    public async handleResponse(res: Response, func: Function) {
+        try {
+            res.send(await func());
+        }catch(ex) {
+            res.status(500).send(ex);
+        } 
+    }
+
     public routes(app): void {
        
         // Contact 
         app.route('/contact') 
         // GET endpoint 
-        .get((req: Request, res: Response) => {
-        // Get all contacts            
-            res.status(200).send({
-                message: 'GET request successfulll!!!!'
-            })
+        .get(async (req: Request, res: Response) => {
+            this.handleResponse(
+                res, 
+                async () => { return this.contactHandler.get({})
+            });
         })        
         // POST endpoint
         .post(async(req: Request, res: Response) => { 
-            
-            try {
-                res.send(await this.contactHandler.create(req.body));
-            } catch(ex) {
-                res.send(ex);
-            }
-
-        // Create new contact         
-            res.status(200).send({
-                message: 'POST request successfulll!!!!'
-            })
+            this.handleResponse(
+                res, 
+                async () => { return this.contactHandler.create(req.body) 
+            });
         })
 
-        // Contact detail
         app.route('/contact/:contactId')
-        // get specific contact
-        .get((req: Request, res: Response) => {
-        // Get a single contact detail            
-            res.status(200).send({
-                message: 'GET request successfulll!!!!'
-            })
+        .get(async (req: Request, res: Response) => {
+            this.handleResponse(
+                res, 
+                async () => { return this.contactHandler.get({ _id: req.params.contactId }) 
+            });
         })
-        .put((req: Request, res: Response) => {
-        // Update a contact           
-            res.status(200).send({
-                message: 'PUT request successfulll!!!!'
-            })
+        .put(async (req: Request, res: Response) => {
+            this.handleResponse(
+                res, 
+                async () => { return this.contactHandler.update(req.params.contactId, req.body) 
+            });
         })
         .delete((req: Request, res: Response) => {       
-        // Delete a contact     
-            res.status(200).send({
-                message: 'DELETE request successfulll!!!!'
-            })
+            this.handleResponse(
+                res, 
+                async () => { return this.contactHandler.delete(req.params.contactId) 
+            });
         })
 
 
